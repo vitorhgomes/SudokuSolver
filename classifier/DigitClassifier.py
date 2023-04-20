7db91f34453a102e1a49a8f7300a8445938210d5
vitorhgomes <33125789+vitorhgomes@users.noreply.github.com>
2023-04-20 20:51:39 -0300

    Add files via upload

---
+++ classifier/DigitClassifier.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class DigitClassifier(nn.Module):
+    def __init__(self):
+        super(DigitClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.fc1 = nn.Linear(1024, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 1024)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
